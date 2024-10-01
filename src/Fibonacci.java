75efcf8c44678895c8985a634ed93eb5487b9619
Bianca Soares <119140865+biancasuarz@users.noreply.github.com>
2024-10-01 10:52:01 -0300

    Update Fibonacci.java

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Fibonacci {
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe um número: ");
+        int numero = scanner.nextInt();
+
+        ArrayList<Integer> fibonacci = new ArrayList<>();
+        fibonacci.add(0);
+        fibonacci.add(1);
+
+        while (true) {
+            int proximo = fibonacci.get(fibonacci.size() - 1) + fibonacci.get(fibonacci.size() - 2);
+            if (proximo > numero) {
+                break;
+            }
+            fibonacci.add(proximo);
+        }
+
+        if (fibonacci.contains(numero)) {
+            System.out.println("O número " + numero + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("O número " + numero + " não pertence à sequência de Fibonacci.");
+        }
+
+        scanner.close();
+    }
 }
