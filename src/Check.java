a5994585f76ceb9b30fad704b6de41834bc31eb2
Bianca Soares <119140865+biancasuarz@users.noreply.github.com>
2024-10-01 10:51:10 -0300

    Update Check.java

--- src/Check.java
+++ src/Check.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Check {
+import java.util.Scanner;
+
+public class Check {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma frase: ");
+        String frase = scanner.nextLine();
+
+        int countA = 0;
+
+        for (char c : frase.toCharArray()) {
+            if (c == 'a' || c == 'A') {
+                countA++;
+            }
+        }
+
+        if (countA > 0) {
+            System.out.println("A letra 'a' aparece " + countA + " vez(es) na frase.");
+        } else {
+            System.out.println("A letra 'a' não está presente na frase.");
+        }
+
+        scanner.close();
+    }
 }
