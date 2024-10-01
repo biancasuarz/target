deb2fe4a4ee1454db71f4531b8239a53df2bc6c4
Bianca Soares <119140865+biancasuarz@users.noreply.github.com>
2024-10-01 10:52:34 -0300

    Update Soma.java

--- src/Soma.java
+++ src/Soma.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Soma {
+public class Soma {
+    public static void main(String[] args) {
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        while (k < indice) {
+            k = k + 1;
+            soma = soma + k;
+        }
+
+        System.out.println("A soma é: " + soma);
+    }
 }
